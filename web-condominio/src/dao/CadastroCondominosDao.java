df39b51cb0947fbab184b2c5fbca686a589c47f4
AndersonLeony <leony_maia@hotmail.com>
2016-11-26 10:35:35 -0200

    Update CadastroCondominosDao.java

--- web-condominio/src/dao/CadastroCondominosDao.java
+++ web-condominio/src/dao/CadastroCondominosDao.java
@@ -1,300 +1,84 @@
-package dao;
+package model;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-
-import model.Despesa;
-import model.Fornecedor;
-import model.Periodicidade;
-import model.TipoDespesa;
-import model.cadastroCondominos;
-
-public class CadastroCondominosDao {
-	private Connection conexao;
+public class CadastroCondominosDados {
+	private String CPF;
+	private String Morador_Nome;
+	private String Morador_Email;
+	private String Morador_DataNascimento;
+	private String Morador_Sindico;
+	private String Morador_Telefone;
+	private String Tipo_morador_idTipo_morador;
 	
-	public CadastroCondominosDao() {
-		this.conexao = ConnectionFactory.getConnection();
+	public String getTipo_morador_idTipo_morador() {
+		return Tipo_morador_idTipo_morador;
 	}
-	
-	public Integer salvar(cadastroCondominos cadastro_condominos) throws SQLException{		
-		PreparedStatement ps = null;		
-		String sql = "INSERT INTO morador values (?,?,?,?,?,?,?)";		
-		try {			
-			this.conexao.setAutoCommit(false); // iniciar transação			
-			ps = this.conexao.prepareStatement(sql);
-			
-			ps.setString(1, cadastro_condominos.getCPF());
-			ps.setString(2,cadastro_condominos.getMorador_Nome());
-			ps.setString(3,cadastro_condominos.getMorador_Email());
-			ps.setString(4,cadastro_condominos.getMorador_DataNascimento());
-			ps.setString(5,cadastro_condominos.getMorador_Sindico());
-			ps.setString(6,cadastro_condominos.getMorador_Telefone());
-			ps.setString(7,cadastro_condominos.getSenha());
-			
-			ps.execute();
-			
-			this.conexao.commit();
-			
-			return UtilDao.ultimoId(ps);
-		} catch (SQLException e) {			
-			e.printStackTrace();
-			try {
-				this.conexao.rollback();
-			} catch (SQLException e1) {
-				e1.printStackTrace();
-			}
-			return null;
-		} finally {			
-			if(ps != null){
-				ps.close();
-			}			
-			this.conexao.setAutoCommit(true);
-		}
-package dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-
-import model.Despesa;
-import model.Fornecedor;
-import model.Periodicidade;
-import model.TipoDespesa;
-import model.CadastroCondominosDados;
-
-public class CadastroCondominosDao {
-	private Connection conexao;
-	
-	public CadastroCondominosDao() {
-		this.conexao = ConnectionFactory.getConnection();
+	public void setTipo_morador_idTipo_morador(String tipo_morador_idTipo_morador) {
+		Tipo_morador_idTipo_morador = tipo_morador_idTipo_morador;
 	}
+
+	private String senha;
 	
-	public void salvar(CadastroCondominosDados cadastro_condominos) throws SQLException{		
-		PreparedStatement ps = null;		
-		String sql = "INSERT INTO morador(CPF, Morador_Nome, Morador_Email, Morador_DataNascimento, Morador_Sindico"
-				+ "Morador_Telefone, senha ) "
-				+ "values (?,?,?,?,?,?,?)";		
-		try {			
-			this.conexao.setAutoCommit(false); // iniciar transaÃ§Ã£o			
-			ps = this.conexao.prepareStatement(sql);
-			
-			ps.setString(1,cadastro_condominos.getCPF());
-			ps.setString(2,cadastro_condominos.getMorador_Nome());
-			ps.setString(3,cadastro_condominos.getMorador_Email());
-			ps.setString(4,cadastro_condominos.getMorador_DataNascimento());
-			ps.setString(5,cadastro_condominos.getMorador_Sindico());
-			ps.setString(6,cadastro_condominos.getMorador_Telefone());
-			ps.setString(7,cadastro_condominos.getSenha());
-			
-			ps.execute();
-			
-			this.conexao.commit();
-			
-		} catch (SQLException e) {			
-			e.printStackTrace();
-			try {
-				this.conexao.rollback();
-			} catch (SQLException e1) {
-				e1.printStackTrace();
-			}
-			
-		} finally {			
-			if(ps != null){
-				ps.close();
-			}			
-			this.conexao.setAutoCommit(true);
-		}
-	}
-	
-	public void alterar(CadastroCondominosDados cadastro_condominos) throws SQLException {		
-		PreparedStatement ps = null;		
-		String sql = "UPDATE morador SET CPF=?,"
-				+ "Morador_Nome=?,"
-				+ "Morador_Email=?,"
-				+ "Morador_DataNascimento=?"
-				+ "Morador_Sindico=?"
-				+ "Morador_Telefone=?"
-				+ "senha=? WHERE id=?";		
-		try {			
-			this.conexao.setAutoCommit(false); // iniciar transaÃ§Ã£o			
-			ps = this.conexao.prepareStatement(sql);
-			
-			ps.setString(1, cadastro_condominos.getCPF());
-			ps.setString(2, cadastro_condominos.getMorador_Nome());
-			ps.setString(3, cadastro_condominos.getMorador_Email());
-			ps.setString(4, cadastro_condominos.getMorador_DataNascimento());
-			ps.setString(5, cadastro_condominos.getMorador_Sindico());
-			ps.setString(6, cadastro_condominos.getMorador_Telefone());
-			ps.setString(7, cadastro_condominos.getSenha());
-			
-			ps.execute();
-			
-			this.conexao.commit();			
-		} catch (SQLException e) {			
-			e.printStackTrace();
-			try {
-				this.conexao.rollback();
-			} catch (SQLException e1) {
-				e1.printStackTrace();
-			}
-		} finally {			
-			if(ps != null){
-				ps.close();
-			}			
-			this.conexao.setAutoCommit(true);
-		}
-	}
-	
-	public void excluir(CadastroCondominosDados cadastro_condominos) throws SQLException{		
-		PreparedStatement ps = null;		
-		String sql = "DELETE FROM morador WHERE CPF=?";		
-		try {			
-			this.conexao.setAutoCommit(false); // iniciar transaÃ§Ã£o			
-			ps = this.conexao.prepareStatement(sql);
-			
-			ps.setString(1, cadastro_condominos.getCPF());
-			ps.execute();
-			
-			this.conexao.commit();			
-		} catch (SQLException e) {			
-			e.printStackTrace();
-			try {
-				this.conexao.rollback();
-			} catch (SQLException e1) {
-				e1.printStackTrace();
-			}
-		} finally {			
-			if(ps != null){
-				ps.close();
-			}			
-			this.conexao.setAutoCommit(true);
-		}
-	}
-	
-	public List<CadastroCondominosDados> listar() throws SQLException {
-		
-		Statement myStmt = null;
-		
-		try {
-			List<CadastroCondominosDados> lista = new ArrayList<CadastroCondominosDados>();
-
-			myStmt = this.conexao.createStatement();
-
-			String sql = "SELECT * FROM morador";
-			ResultSet rs = myStmt.executeQuery(sql);
-
-			while (rs.next()) {
-
-				CadastroCondominosDados cadastro_condominos = new CadastroCondominosDados();
-
-				cadastro_condominos.setCPF(rs.getString("CPF"));
-				cadastro_condominos.setMorador_Nome("Morador_Nome");
-				cadastro_condominos.setCPF(rs.getString("Morador_Email"));
-				cadastro_condominos.setCPF(rs.getString("Morador_DataNascimento"));
-				cadastro_condominos.setCPF(rs.getString("Morador_Sindico"));
-				cadastro_condominos.setCPF(rs.getString("Morador_Telefone"));
-				cadastro_condominos.setCPF(rs.getString("senha"));
-				cadastro_condominos.setCPF(rs.getString("Morador_Adimplente"));
-				cadastro_condominos.setCPF(rs.getString("Tipo_morador_idTipo_morador"));
-
-				lista.add(cadastro_condominos);
-			}
-
-			return lista;
-		} finally {
-			 if(myStmt != null){
-				 myStmt.close();
-			 }
-		}
-		
+	public CadastroCondominosDados(){
 		
 	}
-	
-	public Despesa despesaPorId(Integer id) throws SQLException {
-		
-		DespesaDao dao = new DespesaDao();
-		
-		List<Despesa> lista = dao.listar();
-		
-		for (Despesa despesa : lista) {
-			if(despesa.getId().equals(id)){
-				return despesa;
-			}
-		}
-		
-		return null;
+
+	public String getCPF() {
+		return CPF;
 	}
+
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+
+	public String getMorador_Nome() {
+		return Morador_Nome;
+	}
+
+	public void setMorador_Nome(String morador_Nome) {
+		Morador_Nome = morador_Nome;
+	}
+
+	public String getMorador_Email() {
+		return Morador_Email;
+	}
+
+	public void setMorador_Email(String morador_Email) {
+		Morador_Email = morador_Email;
+	}
+
+	public String getMorador_DataNascimento() {
+		return Morador_DataNascimento;
+	}
+
+	public void setMorador_DataNascimento(String morador_DataNascimento) {
+		Morador_DataNascimento = morador_DataNascimento;
+	}
+
+	public String getMorador_Sindico() {
+		return Morador_Sindico;
+	}
+
+	public void setMorador_Sindico(String morador_Sindico) {
+		Morador_Sindico = morador_Sindico;
+	}
+
+	public String getMorador_Telefone() {
+		return Morador_Telefone;
+	}
+
+	public void setMorador_Telefone(String morador_Telefone) {
+		Morador_Telefone = morador_Telefone;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+	
 }
-
-
-		} finally {			
-			if(ps != null){
-				ps.close();
-			}			
-			this.conexao.setAutoCommit(true);
-		}
-	}
-	
-	public List<cadastroCondominos> listar() throws SQLException {
-		
-		Statement myStmt = null;
-		
-		try {
-			List<cadastroCondominos> lista = new ArrayList<cadastroCondominos>();
-
-			myStmt = this.conexao.createStatement();
-
-			String sql = "SELECT * FROM morador";
-			ResultSet rs = myStmt.executeQuery(sql);
-
-			while (rs.next()) {
-
-				cadastroCondominos cadastro_condominos = new cadastroCondominos();
-
-				cadastro_condominos.setCPF(rs.getString("CPF"));
-				cadastro_condominos.setMorador_Nome("Morador_Nome");
-				cadastro_condominos.setCPF(rs.getString("Morador_Email"));
-				cadastro_condominos.setCPF(rs.getString("Morador_DataNascimento"));
-				cadastro_condominos.setCPF(rs.getString("Morador_Sindico"));
-				cadastro_condominos.setCPF(rs.getString("Morador_Telefone"));
-				cadastro_condominos.setCPF(rs.getString("Morador_Adimplente"));
-				cadastro_condominos.setCPF(rs.getString("Tipo_morador_idTipo_morador"));
-
-				lista.add(cadastro_condominos);
-			}
-
-			return lista;
-		} finally {
-			 if(myStmt != null){
-				 myStmt.close();
-			 }
-		}
-		
-		
-	}
-	
-	public Despesa despesaPorId(Integer id) throws SQLException {
-		
-		DespesaDao dao = new DespesaDao();
-		
-		List<Despesa> lista = dao.listar();
-		
-		for (Despesa despesa : lista) {
-			if(despesa.getId().equals(id)){
-				return despesa;
-			}
-		}
-		
-		return null;
-	}
-}
-
