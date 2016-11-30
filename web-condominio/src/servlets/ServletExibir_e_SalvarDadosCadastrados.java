455a3068c6c9e47680cf4c0601993fb03b2cbe53
AndersonLeony <leony_maia@hotmail.com>
2016-11-30 21:50:18 -0200

    Update ServletExibir_e_SalvarDadosCadastrados.java

--- web-condominio/src/servlets/ServletExibir_e_SalvarDadosCadastrados.java
+++ web-condominio/src/servlets/ServletExibir_e_SalvarDadosCadastrados.java
@@ -1,3 +1,7 @@
+/**Este Servlet é responsável por salvar as informações dos moradores 
+ * que são cadastradas e depois redirecionar estas informações para serem
+ * exibidas por uma página jsp de nome "cadastroCondominosDadosExibidos.jsp"*/
+
 package servlets;
 
 import java.io.IOException;
@@ -76,7 +80,7 @@ public class ServletExibir_e_SalvarDadosCadastrados extends HttpServlet {
 			  hexString.append(String.format("%02X", 0xFF & b));
 			}
 			
-			//senha criptografada vai para uma coluna especÃ­fica no SQL
+			//senha criptografada vai para uma coluna especÃƒÂ­fica no SQL
 			String Morador_Senha_Hash = hexString.toString();
 
 			CadastroCondominosDados cadastro_condominos = new CadastroCondominosDados();
@@ -96,60 +100,11 @@ public class ServletExibir_e_SalvarDadosCadastrados extends HttpServlet {
 				dao.salvar(cadastro_condominos);
 				SimpleDateFormat dataImpressao = new SimpleDateFormat("dd-MM-yyy");
 				
-				request.setAttribute("cadastroCondominos", cadastro_condominos);
+				request.setAttribute("cadastro_condominos", cadastro_condominos);
 				
 				RequestDispatcher rd = request.getRequestDispatcher("cadastroCondominosDadosExibidos.jsp");
 				rd.forward(request, response);
 				
-				/*out.println("<html>");
-				out.println("<head>");
-				out.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\">");
-				out.println("<title>Dados a serem cadastrados</title>");
-				out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"css/menu-estilo.css\">");
-				out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"libs/bootstrap-3.3.7-dist/css/bootstrap.css\">");
-				out.println("</head>");
-				out.println("<body>");
-				out.println("<h1>Cadastro realizado com sucesso!!!</h1>");
-				out.println("<h3>Dados Cadastrados:</h3>");
-				out.println("<br><br>");
-				out.println("<table border=\"1\" class=\"table table-striped\">");
-				out.println("<thead>");
-				out.println("<tr>");
-				out.println("<th>CPF</th>");
-				out.println("<th>Morador</th>");
-				out.println("<th>Email</th>");
-				out.println("<th>Data Nascimento</th>");
-				out.println("<th>É sindico?</th>");
-				out.println("<th>Telefone</th>");
-				out.println("<th>Locatario ou Proprietario</th>");
-				out.println("</th>");
-				out.println("</thead>");
-				out.println("<tbody>");
-				out.println("<tbody>");
-				out.println("<tr>");
-				out.println("<td>"+CPF+"</td>");
-				out.println("<td>"+Morador_Nome+"</td>");
-				out.println("<td>"+Morador_Email+"</td>");
-				out.println("<td>"+dataImpressao.format(Morador_DataNascimento)+"</td>");
-				out.println("<td>"+Morador_Sindico+"</td>");
-				out.println("<td>"+Morador_Telefone+"</td>");
-				out.println("<td>"+Tipo_morador_idTipo_morador+"</td>");
-				out.println("</tr>");
-				out.println("</tbody>");
-				out.println("</table>");
-				out.println("<div class=\"control-group\">"
-				+ "<div class=\"controls\">"
-				+ "<input id=\"voltarTelaAdmin\" onclick=\"telaAdmin()\" class=\"btn\" type=\"submit\" value=\"Voltar Tela Admin\" name=\"voltarTelaAdmin\" />"
-				+ "<input id=\"cadastrarNovoMorador\" href=\"http://localhost:8080/web-condominio/cadastroCondominos.html\" class=\"btn\" type=\"submit\" value=\"Cadastrar Outro Morador\" name=\"cadastrarMorador\" />"
-				+ "</div>"
-				+ "</div>");
-				out.println("<script type=\"text/javascript\">");
-				out.println("function telaAdmin(){");
-				out.println("window.location.href = 'http://localhost:8080/web-condominio/JSP_TelaAdmin.jsp';");
-				out.println("}");
-				out.println("</script>");
-				out.println("</body>");
-				out.println("</html>");*/
 				
 			} catch (SQLException e) {
 				out.println("<h1>Erro no cadastro. Volte a página e tente novamente.</h1>");
